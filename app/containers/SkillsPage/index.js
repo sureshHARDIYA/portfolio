f1ad61f4fdcfd303016905b668fb32c7ed563a35
sureshHARDIYA <itsmeskm99@gmail.com>
2017-09-07 22:34:35 +0545

    Include Page Container

--- app/containers/SkillsPage/index.js
+++ app/containers/SkillsPage/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
+import Wrapper from 'components/PageContainer';
 import H1 from 'components/H1';
 import messages from './messages';
 import List from './List';
 import ListItem from './ListItem';
 import ListItemTitle from './ListItemTitle';
-import Wrapper from './Wrapper';
 
 // eslint-disable-line react/prefer-stateless-function
 export default class SkillsPage extends React.Component {
