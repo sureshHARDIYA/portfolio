ff14d3cecef2d7d07d62b26cd7d00e6670519fdc
Gregory Assasie <gregjarvez@gmail.com>
2017-09-09 20:10:41 +0100

    fix lint errors

--- app/containers/App/geolocation.js
+++ app/containers/App/geolocation.js
@@ -1,1 +1,27 @@
-import axios from 'axios'export const language = new Promise(resolve => {  (function getLanguage() {    function getCountryCode(country){      switch (country){        case country.toLowerCase() === 'india':          return 'in'        case country.toLowerCase() === 'nepal':          return 'ne'        default:          return 'en'      }    }    navigator.geolocation.getCurrentPosition((location) => {      const {longitude: lon, latitude: lat} = location.coords      const url = encodeURI(`http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lon}&username=gregjarvez1_`)      axios.get(url).then(location => {          const countryName = location.data.geonames[0].countryName;          const countryCode = getCountryCode(countryName)          resolve(countryCode);      })    })  }())})
+import axios from 'axios';
+
+export const language = new Promise((resolve) => {
+  (function getLanguage() {
+    function getCountryCode(country) {
+      switch (country) {
+        case country.toLowerCase() === 'india':
+          return 'in';
+        case country.toLowerCase() === 'nepal':
+          return 'ne';
+        default:
+          return 'en';
+      }
+    }
+
+    navigator.geolocation.getCurrentPosition((location) => {
+      const { longitude: lon, latitude: lat } = location.coords;
+      const url = encodeURI(`http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lon}&username=gregjarvez1_`);
+      axios.get(url).then((loc) => {
+        const countryName = loc.data.geonames[0].countryName;
+        const countryCode = getCountryCode(countryName);
+        resolve(countryCode);
+      });
+    });
+  }());
+});
+
