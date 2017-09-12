acacbf1ecb5e192467e158669169247c2a741e01
sureshHARDIYA <itsmeskm99@gmail.com>
2017-09-12 11:05:13 +0545

    Remove unnecessory comment

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,11 +1,3 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
 import Helmet from 'react-helmet';
 import styled from 'styled-components';
