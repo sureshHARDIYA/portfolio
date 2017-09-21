60251b0b69f69e5787f023d28b567cf913be693e
sureshHARDIYA <itsmeskm99@gmail.com>
2017-09-21 15:06:51 +0545

    Move header styles to utils

---
+++ app/components/Header/utils.js
@@ -0,0 +1,34 @@
+export const styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px',
+  },
+  bmBurgerBars: {
+    background: '#fff',
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px',
+  },
+  bmCross: {
+    background: '#bdc3c7',
+  },
+  bmMenu: {
+    background: '#008329',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em',
+  },
+  bmMorphShape: {
+    fill: '#373a47',
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+  },
+};
