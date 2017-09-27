455dacc073191bd6c9a51c9f1832683904173899
sureshHARDIYA <itsmeskm99@gmail.com>
2017-09-27 22:37:51 +0545

    get education title to have bold style

---
+++ app/containers/Education/ListItemTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ListItemParent = styled.strong`
+  font-weight: bold;
+  margin-right: 0.5rem;
+
+  &:after {
+    content: ':';
+  }
+`;
+export default ListItemParent;
